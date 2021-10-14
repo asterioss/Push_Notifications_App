48a010e91cff67935b16d92c85b7226ab9f3713c
Asterios <steliospao1@gmail.com>
2021-10-15 01:26:01 +0300

    push notifications

---
+++ one-signal-notifications/index.js
@@ -0,0 +1,38 @@
+var sendNotification = function(data) {
+    var headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": "Basic YWU5ODI4YzUtNTU0Ni00M2ZiLWFjY2ItZWI3NWE4ZjUyNDll"
+    };
+    
+    var options = {
+      host: "onesignal.com",
+      port: 443,
+      path: "/api/v1/notifications",
+      method: "POST",
+      headers: headers
+    };
+    
+    var https = require('https');
+    var req = https.request(options, function(res) {  
+      res.on('data', function(data) {
+        console.log("Response:");
+        console.log(JSON.parse(data));
+      });
+    });
+    
+    req.on('error', function(e) {
+      console.log("ERROR:");
+      console.log(e);
+    });
+    
+    req.write(JSON.stringify(data));
+    req.end();
+  };
+  
+  var message = {
+    app_id: "1e9efea7-8568-4adb-acff-42527a5855bf",
+    contents: {"en": "Kalhspera Magka"},
+    included_segments: ["Subscribed Users"]
+  };
+  
+  sendNotification(message);
